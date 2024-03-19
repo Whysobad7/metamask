d7f05e2ea51e10c9dc241f4eba4e3e75cb3f291f
Adilet Kasimov <kasimoff17@gmail.com>
2024-03-19 16:29:02 +0300

    ts ignore

--- src/app/utils/web3.ts
+++ src/app/utils/web3.ts
@@ -1,8 +1,11 @@
 import Web3 from 'web3';
 
 export const initWeb3 = async () => {
+	// @ts-ignore: Ignore TypeScript error
 	if (window.ethereum) {
+		// @ts-ignore: Ignore TypeScript error
 	  await window.ethereum.request({ method: 'eth_requestAccounts' });
+	  // @ts-ignore: Ignore TypeScript error
 	  return new Web3(window.ethereum);
 	} else {
 	  throw new Error('MetaMask not detected');
